c04ba167000bb2c9315a21688a6a9d0e557d1c60
vukhu0711 <cuc266742@gmail.com>
2025-11-12 18:33:53 +0700

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -49,18 +49,65 @@ let allGenres = [{
             img: "https://i.ibb.co/hx7KZhcD/music-6.gif",
             id: "music-6",
             src: "https://files.catbox.moe/2a0dun.mp3",
-        }, ]
+        }]
     },
     {
         genre: "Track Lẻ",
         songs: [{
-            name: "Đừng Ai Nhắc Về Cô Ấy",
-            artist: "Future Mix",
-            img: "https://i.ibb.co/KcYnNHth/music-2.gif",
-            id: "music-7",
-            src: "https://files.catbox.moe/xyx091.mp3",
-        }, ]
+                name: "Đừng Ai Nhắc Về Cô Ấy",
+                artist: "Future Mix",
+                img: "https://i.ibb.co/KcYnNHth/music-2.gif",
+                id: "music-7",
+                src: "https://files.catbox.moe/xyx091.mp3",
+            },
+            {
+                name: "Full Track",
+                artist: "MACOL",
+                img: "https://i.ibb.co/KcYnNHth/music-2.gif",
+                id: "music-13",
+                src: "https://files.catbox.moe/xyx091.mp3",
+            }
+        ]
     },
+    {
+        genre: "Nonstop",
+        songs: [{
+                name: "Vẽ Cảnh",
+                artist: "VanhJB",
+                img: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGkwNWJmanExdmxyNXk5MTJmcmlzcW95NGQyd2s2aWQzZWoyNWZvMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XcX3Ae8GSDWN1ciqqi/giphy.gif",
+                id: "music-8",
+                src: "https://files.catbox.moe/n9d4rv.mp3",
+            },
+            {
+                name: "Bay Kem Mix",
+                artist: "VanhJB",
+                img: "https://i.ibb.co/placeholder2.gif",
+                id: "music-9",
+                src: "https://files.catbox.moe/nc8fbe.mp3",
+            },
+            {
+                name: "2h Ketamin",
+                artist: "Mix",
+                img: "https://i.ibb.co/placeholder3.gif",
+                id: "music-10",
+                src: "https://files.catbox.moe/uru2ru.mp3",
+            },
+            {
+                name: "Nonstop Love Songs",
+                artist: "DJ Hoàng Anh",
+                img: "https://i.ibb.co/placeholder4.gif",
+                id: "music-11",
+                src: "https://files.catbox.moe/sample4.mp3",
+            },
+            {
+                name: "Nonstop Best Hits",
+                artist: "DJ Quang Đăng",
+                img: "https://i.ibb.co/placeholder5.gif",
+                id: "music-12",
+                src: "https://files.catbox.moe/sample5.mp3",
+            }
+        ]
+    }
 ];
 
 // Biến allMusic hiện tại sẽ được khởi tạo (tải) bằng thể loại đầu tiên
